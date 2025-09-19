9d30f72af9a53a6c1893bf062741c3102ab42270
ajogious <ajogious@gmail.com>
2025-09-19 13:32:46 +0100

    copyright year updated to dynamic

--- js/main.js
+++ js/main.js
@@ -1,139 +1,140 @@
 (function ($) {
-	('use strict');
+  ("use strict");
+  // copyright year
+  document.getElementById("year").textContent = new Date().getFullYear();
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
+      }
+    }, 1);
+  };
+  spinner();
 
-	// Spinner
-	var spinner = function () {
-		setTimeout(function () {
-			if ($('#spinner').length > 0) {
-				$('#spinner').removeClass('show');
-			}
-		}, 1);
-	};
-	spinner();
+  // Initiate the wowjs
+  new WOW().init();
 
-	// Initiate the wowjs
-	new WOW().init();
+  // Sticky Navbar
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    } else {
+      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
+    }
+  });
 
-	// Sticky Navbar
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 300) {
-			$('.sticky-top').addClass('shadow-sm').css('top', '0px');
-		} else {
-			$('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
-		}
-	});
+  // updating the ime at navigation
+  function updateTime() {
+    const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const now = new Date();
+    const dayName = days[now.getDay()];
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    const formattedHours = hours % 12 || 12;
+    const formattedMinutes = minutes.toString().padStart(2, "0");
 
-	// updating the ime at navigation
-	function updateTime() {
-		const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-		const now = new Date();
-		const dayName = days[now.getDay()];
-		const hours = now.getHours();
-		const minutes = now.getMinutes();
-		const ampm = hours >= 12 ? 'PM' : 'AM';
-		const formattedHours = hours % 12 || 12;
-		const formattedMinutes = minutes.toString().padStart(2, '0');
+    $("#day").text(`${dayName} - `);
+    $("#time").text(`${formattedHours}:${formattedMinutes} ${ampm}`);
+  }
 
-		$('#day').text(`${dayName} - `);
-		$('#time').text(`${formattedHours}:${formattedMinutes} ${ampm}`);
-	}
+  $(document).ready(function () {
+    setInterval(updateTime, 1000);
+    updateTime();
+  });
 
-	$(document).ready(function () {
-		setInterval(updateTime, 1000);
-		updateTime();
-	});
+  // updating the time at navigation end
 
-	// updating the time at navigation end
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
+    return false;
+  });
 
-	// Back to top button
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 300) {
-			$('.back-to-top').fadeIn('slow');
-		} else {
-			$('.back-to-top').fadeOut('slow');
-		}
-	});
-	$('.back-to-top').click(function () {
-		$('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
-		return false;
-	});
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
 
-	// Facts counter
-	$('[data-toggle="counter-up"]').counterUp({
-		delay: 10,
-		time: 2000,
-	});
+  // Header carousel
+  $(".header-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1500,
+    items: 1,
+    dots: true,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-chevron-left"></i>',
+      '<i class="bi bi-chevron-right"></i>',
+    ],
+  });
 
-	// Header carousel
-	$('.header-carousel').owlCarousel({
-		autoplay: true,
-		smartSpeed: 1500,
-		items: 1,
-		dots: true,
-		loop: true,
-		nav: true,
-		navText: [
-			'<i class="bi bi-chevron-left"></i>',
-			'<i class="bi bi-chevron-right"></i>',
-		],
-	});
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
+    autoplay: true,
+    smartSpeed: 1000,
+    center: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    navText: [
+      '<i class="bi bi-arrow-left"></i>',
+      '<i class="bi bi-arrow-right"></i>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+    },
+  });
 
-	// Testimonials carousel
-	$('.testimonial-carousel').owlCarousel({
-		autoplay: true,
-		smartSpeed: 1000,
-		center: true,
-		dots: false,
-		loop: true,
-		nav: true,
-		navText: [
-			'<i class="bi bi-arrow-left"></i>',
-			'<i class="bi bi-arrow-right"></i>',
-		],
-		responsive: {
-			0: {
-				items: 1,
-			},
-			768: {
-				items: 2,
-			},
-		},
-	});
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
 
-	// Portfolio isotope and filter
-	var portfolioIsotope = $('.portfolio-container').isotope({
-		itemSelector: '.portfolio-item',
-		layoutMode: 'fitRows',
-	});
-	$('#portfolio-flters li').on('click', function () {
-		$('#portfolio-flters li').removeClass('active');
-		$(this).addClass('active');
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
 
-		portfolioIsotope.isotope({ filter: $(this).data('filter') });
-	});
+  const paymentForm = document.getElementById("paymentForm");
+  paymentForm.addEventListener("submit", payWithPaystack, false);
 
-	const paymentForm = document.getElementById('paymentForm');
-	paymentForm.addEventListener('submit', payWithPaystack, false);
+  function payWithPaystack(e) {
+    e.preventDefault();
 
-	function payWithPaystack(e) {
-		e.preventDefault();
+    let handler = PaystackPop.setup({
+      key: "pk_test_506cea3e8058434021b7655d3c41406f88220df3", // Replace with your public key
+      email: document.getElementById("email-address").value,
+      amount: document.getElementById("amount").value * 100, // Convert to kobo
+      ref: "" + Math.floor(Math.random() * 1000000000 + 1), // Generate pseudo-unique reference
+      label: "Donation from " + document.getElementById("name").value,
+      onClose: function () {
+        alert("Window closed.");
+      },
+      callback: function (response) {
+        let message = "Payment complete! Reference: " + response.reference;
+        alert(message);
+        // You can also handle post-payment actions here (e.g., save the payment details to your database)
+      },
+    });
 
-		let handler = PaystackPop.setup({
-			key: 'pk_test_506cea3e8058434021b7655d3c41406f88220df3', // Replace with your public key
-			email: document.getElementById('email-address').value,
-			amount: document.getElementById('amount').value * 100, // Convert to kobo
-			ref: '' + Math.floor(Math.random() * 1000000000 + 1), // Generate pseudo-unique reference
-			label: 'Donation from ' + document.getElementById('name').value,
-			onClose: function () {
-				alert('Window closed.');
-			},
-			callback: function (response) {
-				let message = 'Payment complete! Reference: ' + response.reference;
-				alert(message);
-				// You can also handle post-payment actions here (e.g., save the payment details to your database)
-			},
-		});
-
-		handler.openIframe();
-	}
+    handler.openIframe();
+  }
 })(jQuery);
